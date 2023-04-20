e19f4e290e8d27161ed34a24543c2bd4fde3c553
saro3108 <34979653+saro3108@users.noreply.github.com>
2023-04-20 16:27:44 +0530

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+<div v-for="item in data" :key="item.id">
+  <div>{{item.title}}</div>
+  <div>{{item.description}}</div>
+</div>
+</div>
+ 
+</template>
+
+<script setup>
+// import QuestionValue from './question.json'
+
+const data =  [
+    {
+       "title": "Clean the kitchen",
+       "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+    },
+    {
+       "title": "Call Mom",
+       "description": "It's her birthday!"
+    },
+    {
+       "title": "Water flowers",
+       "description": "They need water, or they will die."
+    },
+    {
+        "title": "Clean the kitchen",
+        "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+     },
+     {
+        "title": "Call Mom",
+        "description": "It's her birthday!"
+     },
+     {
+        "title": "Water flowers",
+        "description": "They need water, or they will die."
+     },
+     {
+        "title": "Clean the kitchen",
+        "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+     },
+     {
+        "title": "Call Mom",
+        "description": "It's her birthday!"
+     },
+     {
+        "title": "Water flowers",
+        "description": "They need water, or they will die."
+     }
+ ] 
+</script>
+
+
+
